7e9ec33e036af2811df1ab5d868b5773fe38bfa7
MilosR <10090642+MilosR@users.noreply.github.com>
2018-12-27 18:06:36 +0100

    Protocol fix for BrowserSync

--- generators/app/templates/theme/footer.php
+++ generators/app/templates/theme/footer.php
@@ -2,7 +2,7 @@
 
 <?php if(defined("WPSITE_DEV") && WPSITE_DEV == true): ?>
     <script id="__bs_script__">//<![CDATA[
-        document.write("<script async src='https://HOST:3000/browser-sync/browser-sync-client.js?v=2.23.6'><\/script>".replace("HOST", location.hostname));
+        document.write("<script async src='//HOST:3000/browser-sync/browser-sync-client.js?v=2.23.6'><\/script>".replace("HOST", location.hostname));
         //]]></script>
 <?php endif; ?>
 
